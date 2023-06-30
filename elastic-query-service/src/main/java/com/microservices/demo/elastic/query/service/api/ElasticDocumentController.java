6bf2e8f8c702cfef9544bd8b2b5c95317c07f7e0
zahid <zahidsaleem0440@gmail.com>
2023-06-30 13:00:28 +0500

    - Created new branch from the master branch. The intent of creating new branch is to test the uri based versioning technique. Here we have made a breaking change (meaning we know have to upgrade the major version number; remember semantic based versioning where we have major.minor.patch based versioning schema). - The breaking change: We intend to change the String id field in ElasticQueryServiceResponseModel to Long. This will break the client sa client expects to have String based field and he will get Long return. In order to do that we have created a  new class ElasticQueryServiceResponseModelV2 where every type is same as ElasticQueryServiceResponseModel except id field. In order to compensate for that we have created a new wrapper method in ElasticDocumentController which returns ElasticQueryServiceResponseModelV2 object and uses getV2Model method to wrap the ElasticQueryServiceResponseModel to V2 model. Moreover we have also provided the version v1 and v2 in the endpoint URI's. Test: In order to test run elastic cluster "docker-compose -f common.yml -f elastic_cluster.yml up" & config-server & elastic-query-service. First access all documents using URI "http://localhost:8183/elastic-query-service/documents/v1" & then select a single ID and access both getDocumentById & getDocumentByIdV2 methods using "http://localhost:8183/elastic-query-service/documents/v1/ID" & "http://localhost:8183/elastic-query-service/documents/v2/ID". Observe the difference in ID fields of both.

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.microservices.demo.elastic.query.service.api;
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +24,13 @@ public class ElasticDocumentController {
         this.elasticQueryService = queryService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/v1")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments(){
         List<ElasticQueryServiceResponseModel> response = elasticQueryService.getAllDocuments();
         LOG.info("Elasticsearch returned {} of documents", response.size());
         return ResponseEntity.ok(response);
     }
-    @GetMapping("/{id}")
+    @GetMapping("/v1/{id}")
     public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id){
         ElasticQueryServiceResponseModel    elasticQueryServiceResponseModel =
                 elasticQueryService.getDocumentById(id);
@@ -37,7 +38,16 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
-    @PostMapping("/get-document-by-text")
+    @GetMapping("/v2/{id}")
+    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2
+            (@PathVariable @NotEmpty String id){
+        ElasticQueryServiceResponseModel    responseModel =
+                elasticQueryService.getDocumentById(id);
+        LOG.debug("Elasticsearch returned document with id {}", id);
+        ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(responseModel);
+        return ResponseEntity.ok(responseModelV2);
+    }
+    @PostMapping("/v1/get-document-by-text")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>>
     getDocumentsByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel){
         //@Valid annotation processes validation for all validation annotations on the object e.g. @NotEmpty assigned
@@ -46,5 +56,16 @@ public class ElasticDocumentController {
                 elasticQueryService.getDocumentByText( elasticQueryServiceRequestModel.getText());
          LOG.debug("Elasticsearch returned {} documents", response.size());
         return ResponseEntity.ok(response);
+    }
+
+    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel){
+        ElasticQueryServiceResponseModelV2 responseModelV2 =
+                ElasticQueryServiceResponseModelV2.builder().
+                        id(Long.parseLong(responseModel.getId())).
+                        text(responseModel.getText()).
+                        userId(responseModel.getUserId()).
+                        createdAt(responseModel.getCreatedAt()).build();
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
     }
 }
---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/ElasticQueryServiceResponseModelV2.java
@@ -0,0 +1,22 @@
+package com.microservices.demo.elastic.query.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryServiceResponseModelV2 extends RepresentationModel<ElasticQueryServiceResponseModelV2> {
+    //The added extension will change the Response Object to a Rest Representation model. This will be helpful in
+    //setting hateoas related links on the response object.
+    private Long id;
+    private String text;
+    private Long userId;
+    private LocalDateTime createdAt;
+}
